40e3ce368940d11b27a143e4d68e7807d9831b8f
srjuchenko <srjuchenko@gmail.com>
2023-02-04 15:17:33 +0200

    finished total expense component

---
+++ src/components/TotalExpenses/TotalExpenses.js
@@ -0,0 +1,19 @@
+import "./TotalExpenses.css";
+
+/**
+ *
+ * @param {number} total expenses
+ *
+ * @returns component that shows the total expenses
+ */
+
+function TotalExpenses(props) {
+  return (
+    <div className="total-expenses">
+      <h1 className="total">Total Expenses:</h1>
+      <h2 className="expenses">{props.expenses}$</h2>
+    </div>
+  );
+}
+
+export default TotalExpenses;
