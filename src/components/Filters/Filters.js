259e2cf6e1dd4de6990bd632ee0eae62b9dc3492
Moshe Frankipour <moshe.frankipour@intel.com>
2023-02-06 22:30:31 +0200

    fix filter (#13)

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,54 +1,82 @@
 import { useState } from 'react';
-import { Button, ButtonGroup } from "@mui/material";
+import { Button, ButtonGroup, FormControl, FormControlLabel, InputLabel, MenuItem, Select } from "@mui/material";
 import Storage from "../../utils/Storage";
-import TextField from '@mui/material/TextField';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import "./Filters.css"; 
 
-const clear = async(onUpdateExpense) => {
+const clear = async(onUpdateExpense, setValue) => {
     const data = await Storage.getItems()
     onUpdateExpense(data)
+    setValue({year: -1, month: -1})
 }
 
 const updateFilter = async(onUpdateExpense, value) => {
     let data = await Storage.getItems()
-    const date = new Date(value)
-    data = data.filter(d => new Date(d.date).getYear() == date.getYear() 
-                            && new Date(d.date).getMonth() == date.getMonth());
+    if (value.year != -1)
+        data = data.filter(d => new Date(d.date).getYear() == value.year);
+    if (value.month != -1)
+        data = data.filter(d=> new Date(d.date).getMonth() == value.month);
     onUpdateExpense(data)
 }
 
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+const YEARS =
+    Array.from(
+    { length: (2030 - 1950) / 1 + 1 },
+    (value, index) => 1950 + index * 1
+    );
+
 function Filters(props) {
-    const [value, setValue] = useState(dayjs('2023-04'));
+    const [value, setValue] = useState({year: -1, month: -1});
     return (
         <div className="filters">
-            <h1>Filters</h1>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                    views={['year']}
+            <h3>Filter By Year And Month</h3>
+            <FormControl>
+                <InputLabel id="Year-label">Year</InputLabel>
+                <Select
+                    labelId="Year-label"
+                    id="Year"
+                    value={value.year}
                     label="Year"
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
+                    onChange={(event) => {
+                        setValue({year: event.target.value , month: value.month});
                     }}
-                    renderInput={(params) => <TextField {...params} helperText={null} />}
-                />
-                <DatePicker
-                    views={['month']}
+                >
+                    <MenuItem value="-1">Empty</MenuItem>
+                    {YEARS.map((x)=> <MenuItem value={x}>{x}</MenuItem>)}
+                </Select>
+            </FormControl>
+            <FormControl>
+            <InputLabel id="month-label">Month</InputLabel>
+                <Select
+                    labelId="month-label"
+                    id="month"
+                    value={value.month}
                     label="Month"
-                    value={value}
-                    onChange={(newValue) => {
-                        setValue(newValue);
+                    onChange={(event) => {
+                        setValue({year: value.year, month: event.target.value});
                     }}
-                    renderInput={(params) => <TextField {...params} helperText={null} />}
-                />
-            </LocalizationProvider>
+                >
+                    <MenuItem value="-1">Empty</MenuItem>
+                    {MONTHS.map((x, index)=> <MenuItem value={index}>{x}</MenuItem>)}
+                </Select>
+            </FormControl>
             <ButtonGroup variant="outlined" aria-label="outlined button group">
                 <Button onClick={() => updateFilter(props.onUpdateExpense, value)}>Filter</Button>
-                <Button onClick={() => clear(props.onUpdateExpense)}>Clear</Button>
+                <Button onClick={() => clear(props.onUpdateExpense, setValue)}>Show All</Button>
             </ButtonGroup>
         </div>
       );
